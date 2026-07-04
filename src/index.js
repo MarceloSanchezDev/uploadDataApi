ee5d2c6f5abc6aac3c7e8e4899d9872e1d2d83cc
Marcelo Sanchez <marcedgardosanchez@gmail.com>
2026-07-04 05:21:35 -0300

    Update CORS configuration to allow requests from the production frontend URL

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  },
+  cors(
 {
     origin: "https://front-upload-data-api.vercel.app/",
     credentials: true,
